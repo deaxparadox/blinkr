4c82981c58a8af3b88b92ff8c9ff468dbd5d008e
deaxparadox <nitish136900kushwaha@gmail.com>
2024-12-19 01:49:31 +0530

    working production

--- shorty/shorty/settings/prod.py
+++ shorty/shorty/settings/prod.py
@@ -1,3 +1,5 @@
+from .base import *
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-6=d3kw1ny$^cj7g_^bz&=0dcoh=jr$u*kddy#hhts%nya=)uhe'
 
@@ -5,3 +7,4 @@ SECRET_KEY = 'django-insecure-6=d3kw1ny$^cj7g_^bz&=0dcoh=jr$u*kddy#hhts%nya=)uhe
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
+
