bf1fc636b41411cc9e9708f8c7a796ae245123a1
deaxparadox <nitish136900kushwaha@gmail.com>
2024-12-19 22:17:27 +0530

    test url model

---
+++ shorty/shortener/tests/test_url_model.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from shortener.models import URL, URLEncodeMedium
+
+
+class TestURLModel(TestCase):
+    def setUp(self):
+        self.new_url = URL.objects.create(full_url="https://google.com/1", medium=URLEncodeMedium.NORMAL)
+        
+    def test_url_instance_default_value(self):
+        self.assertEqual(self.new_url.active, True)
+        self.assertEqual(self.new_url.delete, False)
+        self.assertEqual(self.new_url.clicks, 0)
+        self.assertEqual(self.new_url.visibility, 1)
+        self.assertEqual(self.new_url.visibility.value, 1)
+        self.assertEqual(self.new_url.visibility.name, "PUBLIC")
+        
